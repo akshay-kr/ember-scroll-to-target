f8b197875210c094c3fc4f09421905637490222b
Akshay Kumar <akshaykumar.cse@gmail.com>
2018-01-14 21:55:16 +0530

    Integrated travis build

--- app/components/scroll-to.js
+++ app/components/scroll-to.js
@@ -1,4 +1,3 @@
-import Ember from "ember";
 import scrollTo from "ember-scroll-to-target/components/scroll-to";
 
 export default scrollTo;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,11 @@
 /* eslint-env node */
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Add options here
+    "ember-cli-babel": {
+      includePolyfill: true
+    }
   });
 
   /*
