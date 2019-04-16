35f371309b3482e663180d1dea7631dc7e1ecdff
Kumar <akshaykumar.cse@gmail.com>
2019-04-16 21:46:44 +0530

    Refactored and removed usage of JQuery

--- addon/components/scroll-to.js
+++ addon/components/scroll-to.js
@@ -15,17 +15,38 @@ export default Ember.Component.extend({
       Ember.Logger.error("Target should be passed");
       return;
     }
-    $(`#${this.get("elementId")}`).on("click", () => {
-      self.scrollToTarget(target, offset, duration);
-    });
+    document.querySelector(`#${this.get("elementId")}`)
+      .addEventListener("click", function () {
+        self.scrollToTarget(target, offset, duration);
+      });
   },
 
   scrollToTarget(target, offset, duration) {
-    $("html, body").animate(
-      {
-        scrollTop: $(target).offset().top + offset
-      },
-      duration
-    );
+    const targetPos = document.querySelector(target).offsetTop + offset;
+    this.animateScroll(targetPos, duration)
+  },
+
+  animateScroll(targetPos, duration) {
+    const self = this;
+    const startPos = window.scrollY;
+    const speed = 20;
+    let time = 0;
+    const animate = function () {
+      time += speed;
+      window.scrollTo(0, self.getAnimationPos(time, startPos, targetPos - startPos, duration));
+      if (time < duration) {
+        setTimeout(animate, speed);
+      }
+    };
+    animate();
+  },
+
+  getAnimationPos(time, startPos, endPos, duration) {
+    time /= duration / 2;
+    if (time < 1) {
+      return endPos / 2 * time * time + startPos;
+    }
+    time--;
+    return -endPos / 2 * (time * (time - 2) - 1) + startPos;
   }
 });
